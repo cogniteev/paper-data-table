2cd0236f1792c636729da28dcc49060ffd3a1234
Anthony Pessy <anthony@cogniteev.com>
2017-12-18 14:56:51 +0100

    expose 'disabled' attribute on table row

--- addon/components/paper-data-table-row.js
+++ addon/components/paper-data-table-row.js
@@ -11,6 +11,7 @@ export default Component.extend({
 	layout,
 	tagName: 'tr',
 	classNames: ['md-row'],
+  classNameBindings: ['disabled:md-disabled'],
 	showEdit: false,
 	attributeBindings: ['style'],
 	style: computed('edit', 'onClick', function() {
